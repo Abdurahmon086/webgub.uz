aa52a521ce7dffeaa8e581dddc7cb25720f3e2a9
Safarmurod999 <supermen0904@gmail.com>
2023-11-02 21:49:53 +0500

    fixed

--- src/components/Aside/aside.jsx
+++ src/components/Aside/aside.jsx
@@ -11,12 +11,12 @@ function Aside({ menus }) {
     return (
         <aside style={(menus == true ? { display: "none" } : { display: 'block' })} className='bg-white min-w-[300px] py-4 px-3 shadow-lg'>
             <ul className='flex flex-col gap-1'>
-                <li className='flex gap-3 bg-blue-100 rounded py-3 px-2 active:bg-blue-500 active:text-white transition-all'><Link href='/dashboard' className='flex gap-2 items-center'><Image src={home} className='w-[24px] h-[24px]'/> <p className='font-bold text-xl'>Dashboard</p></Link></li>
-                <li className='flex gap-3 bg-blue-100 rounded py-3 px-2 active:bg-blue-500 active:text-white transition-all'><Link href='/dashboard/students' className='flex gap-2 items-center'><Image src={user} className='w-[24px] h-[24px]'/> <p className='font-bold text-xl'>O'quvchilar</p></Link></li>
-                <li className='flex gap-3 bg-blue-100 rounded py-3 px-2 active:bg-blue-500 active:text-white transition-all'><Link href='/dashboard/kurslar' className='flex gap-2 items-center'><Image src={book} className='w-[24px] h-[24px]'/> <p className='font-bold text-xl'>Kurslar</p></Link></li>
-                <li className='flex gap-3 bg-blue-100 rounded py-3 px-2 active:bg-blue-500 active:text-white transition-all'><Link href='/dashboard/buyurtmachilar' className='flex gap-2 items-center'><Image src={userBadge} className='w-[24px] h-[24px]'/> <p className='font-bold text-xl'>Buyurtmachilar</p></Link></li>
-                <li className='flex gap-3 bg-blue-100 rounded py-3 px-2 active:bg-blue-500 active:text-white transition-all'><Link href='/dashboard/xizmatlar' className='flex gap-2 items-center'> <Image src={car} className='w-[24px] h-[24px]'/> <p className=' font-bold text-xl'>Xizmatlar</p></Link></li>
-        </ul>
+                <li className='flex gap-3 bg-blue-100 rounded py-3 px-2 active:bg-blue-500 active:text-white transition-all'><Link href='/dashboard' className='flex w-full h-full gap-2 items-center'><Image src={home} className='w-[24px] h-[24px]' /> <p className='font-bold text-xl'>Dashboard</p></Link></li>
+                <li className='flex gap-3 bg-blue-100 rounded py-3 px-2 active:bg-blue-500 active:text-white transition-all'><Link href='/dashboard/students' className='flex w-full h-full  gap-2 items-center'><Image src={user} className='w-[24px] h-[24px]' /> <p className='font-bold text-xl'>O'quvchilar</p></Link></li>
+                <li className='flex gap-3 bg-blue-100 rounded py-3 px-2 active:bg-blue-500 active:text-white transition-all'><Link href='/dashboard/kurslar' className='flex w-full h-full  gap-2 items-center'><Image src={book} className='w-[24px] h-[24px]' /> <p className='font-bold text-xl'>Kurslar</p></Link></li>
+                <li className='flex gap-3 bg-blue-100 rounded py-3 px-2 active:bg-blue-500 active:text-white transition-all'><Link href='/dashboard/buyurtmachilar' className='flex w-full h-full  gap-2 items-center'><Image src={userBadge} className='w-[24px] h-[24px]' /> <p className='font-bold text-xl'>Buyurtmachilar</p></Link></li>
+                <li className='flex gap-3 bg-blue-100 rounded py-3 px-2 active:bg-blue-500 active:text-white transition-all'><Link href='/dashboard/xizmatlar' className='flex w-full h-full  gap-2 items-center'> <Image src={car} className='w-[24px] h-[24px]' /> <p className=' font-bold text-xl'>Xizmatlar</p></Link></li>
+            </ul>
         </aside >
     )
 }
